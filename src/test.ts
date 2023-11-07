2a5949c9aef0cf3aaef90317624fce2cb607614c
Andreas Korth <andreas.korth@gmail.com>
2023-11-07 14:28:25 +0100

    wip: relations

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,52 @@
-import 'dotenv/config'
-import './luxon.inspect.custom'
-import Session from './session'
-import { configureSupamodel } from '.'
+class Many<T> extends Array<T> implements PromiseLike<T> {
+  private fetch: () => Promise<T[]>
+  private result: ReturnType<typeof this.fetch>
+  loaded = false
+  loading = false
 
-configureSupamodel({})
+  constructor(fetch: () => Promise<T[]>, preload = true) {
+    super()
+    this.fetch = fetch
+    this.result = Promise.resolve([])
+    if (preload) this.load()
+  }
 
-Session.signInWithPassword({
-  email: 'andreas.korth@gmail.com',
-  password: 'rosebush',
-}).then(async (session) => {
-  console.dir(session.toJSON(), { depth: 5 })
-})
+  load() {
+    if (this.loading) return this.result
+
+    this.loading = true
+
+    this.result = this.fetch()
+      .then((data) => {
+        this.length = 0
+        this.push(...data)
+        this.loaded = true
+        return data
+      })
+      .finally(() => {
+        this.loading = false
+      })
+    return this.result
+  }
+
+  then<TResult1 = typeof this>(...args: Parameters<Promise<TResult1>['then']>) {
+    if (!this.loaded && !this.loading) {
+      this.load()
+    }
+    this.result = this.result.then(...(args as any))
+    return this
+  }
+}
+
+const many = new Many(() =>
+  fetch('https://654a17d6e182221f8d527c74.mockapi.io/users').then<[]>(
+    (response) => response.json(),
+  ),
+)
+
+;(async function () {
+  console.log(many.loaded)
+  console.log(await many)
+  console.log(many.loaded)
+  console.log(many)
+})()
